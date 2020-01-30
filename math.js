4e67761934645b8ea90004c5d0d606e0380cfd5d
benjorama <benmacomb@gmail.com>
2020-01-30 16:24:08 -0700

    Implement simple math functions.

--- math.js
+++ math.js
@@ -6,3 +6,30 @@
 function add( a, b ) {
   return a + b;
 }
+
+/**
+ * Return the difference of two numbers. 
+ * @param {number} a 
+ * @param {number} b 
+ */
+function subtract( a, b ) {
+  return a - b;
+}
+
+/**
+ * Return the product of two numbers.
+ * @param {number} a 
+ * @param {number} b 
+ */
+function multiply( a, b ) {
+  return a * b;
+}
+
+/**
+ * Return the quotient of two numbers. 
+ * @param {number} a 
+ * @param {number} b 
+ */
+function divide( a, b ) {
+  return a / b;
+}
