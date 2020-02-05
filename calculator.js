4d22977de9c6d92cdac5bea21dc316a38461f605
benjorama <benmacomb@gmail.com>
2020-02-05 14:22:43 -0700

    Save initial value 'a' after clicking an operator.

--- calculator.js
+++ calculator.js
@@ -1,12 +1,18 @@
-let number = '';
+let a = "";
+let operator = "";
 
 function initKeys() {
   const keys = document.querySelectorAll( ".key" );
+  const operators = /\+|\*|\/|\-/;
   keys.forEach( element => {
     element.addEventListener( "click", () => {
       const text = document.querySelector( ".displayText" );
-      number += element.innerHTML;
-      text.innerHTML = number;
+      if ( !operators.test( element.innerHTML ) )
+        a += element.innerHTML;
+      else {
+        operator += element.innerHTML;
+      }
+      text.innerHTML = a + operator;
     });
   });
 }
