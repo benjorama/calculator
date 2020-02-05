aa2fabe4d1e713668a7d57dac30fa5b0b902b2d1
benjorama <benmacomb@gmail.com>
2020-02-05 15:19:57 -0700

    Save value 'b' after clicking '=' button

--- calculator.js
+++ calculator.js
@@ -1,18 +1,26 @@
 let a = "";
+let b = "";
 let operator = "";
+let firstOperand = true;
 
 function initKeys() {
   const keys = document.querySelectorAll( ".key" );
-  const operators = /\+|\*|\/|\-/;
+  const text = document.querySelector( ".displayText" );
+  const operators = /\+|\*|\/|\-|\=/;
   keys.forEach( element => {
     element.addEventListener( "click", () => {
-      const text = document.querySelector( ".displayText" );
-      if ( !operators.test( element.innerHTML ) )
+      if ( !operators.test( element.innerHTML ) && firstOperand )
         a += element.innerHTML;
+      else if ( !operators.test( element.innerHTML ) && !firstOperand )
+        b += element.innerHTML;
       else {
-        operator += element.innerHTML;
+        if ( element.innerHTML != "=") {
+          operator += element.innerHTML;
+          firstOperand = false;
+        } else
+          console.log( b );
       }
-      text.innerHTML = a + operator;
+      text.innerHTML = a + operator + b;
     });
   });
 }
