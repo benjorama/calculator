06b6e21879ba4f168f9672083bd692463a593705
benjorama <benmacomb@gmail.com>
2020-02-09 11:50:32 -0700

    When equal sign is clicked, calculator evaluates a valid infix expression and displays the result.

--- calculator.js
+++ calculator.js
@@ -1,28 +1,17 @@
-let a = "";
-let b = "";
-let operator = "";
-let firstOperand = true;
+import evaluateExpression from "./evaluateExpression.js";
 
-function initKeys() {
-  const keys = document.querySelectorAll( ".key" );
-  const text = document.querySelector( ".displayText" );
-  const operators = /\+|\*|\/|\-|\=/;
-  keys.forEach( element => {
-    element.addEventListener( "click", () => {
-      if ( !operators.test( element.innerHTML ) && firstOperand )
-        a += element.innerHTML;
-      else if ( !operators.test( element.innerHTML ) && !firstOperand )
-        b += element.innerHTML;
-      else {
-        if ( element.innerHTML != "=") {
-          operator += element.innerHTML;
-          firstOperand = false;
-        } else
-          console.log( b );
-      }
-      text.innerHTML = a + operator + b;
-    });
+let expression = "";
+const keys = document.querySelectorAll( ".key" );
+const text = document.querySelector( ".displayText" );
+keys.forEach( key => {
+  key.addEventListener( "click", () => {
+    if ( key.innerHTML != "=" ) {
+      expression += key.innerHTML + " ";
+      text.innerHTML = expression;
+    }
+    if ( key.innerHTML === "=" ) {
+      expression = expression.trim();
+      text.innerHTML = evaluateExpression( expression );
+    }
   });
-}
-
-initKeys();
+});
