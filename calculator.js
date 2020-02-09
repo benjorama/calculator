b3b86d90f1111aabc6cb874130a0c0d33b11e272
benjorama <benmacomb@gmail.com>
2020-02-09 12:23:32 -0700

    Support numbers more than one digit long, allow for continuing calculations after a result.

--- calculator.js
+++ calculator.js
@@ -1,21 +1,19 @@
 import evaluateExpression from "./evaluateExpression.js";
 
 let expression = "";
+const operators = /\+|\-|\*|\//;
 const keys = document.querySelectorAll( ".key" );
 const text = document.querySelector( ".displayText" );
 keys.forEach( key => {
   key.addEventListener( "click", () => {
-    if ( key.innerHTML != "=" ) {
-      expression += key.innerHTML + " ";
-      text.innerHTML = expression;
-    }
-    if ( key.innerHTML === "=" ) {
-      expression = expression.trim();
-      text.innerHTML = evaluateExpression( expression );
-    }
-    if ( key.innerHTML === "C" ) {
+    if ( key.innerHTML != "=" && !operators.test( key.innerHTML ) ) 
+      expression += key.innerHTML;
+    if ( operators.test( key.innerHTML ) )
+      expression += " " + key.innerHTML + " ";
+    if ( key.innerHTML === "=" ) 
+      expression = evaluateExpression( expression );
+    if ( key.innerHTML === "C" ) 
       expression = "";
-      text.innerHTML = "";
-    }
+    text.innerHTML = expression;
   });
 });
