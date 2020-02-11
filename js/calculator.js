c3dcc2894fd7ceb50ca95d46b2fdcac2930eeb2f
benjorama <benmacomb@gmail.com>
2020-02-10 17:47:30 -0700

    Don't allow user to click two consecutive operators.

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,7 @@
 import evaluateExpression from "./evaluateExpression.js";
 
 let expression = "";
+let previousKey = "";
 const operators = /\+|\-|\*|\//;
 const keys = document.querySelectorAll( ".key" );
 const text = document.querySelector( ".displayText" );
@@ -8,12 +9,13 @@ keys.forEach( key => {
   key.addEventListener( "click", () => {
     if ( key.innerHTML != "=" && !operators.test( key.innerHTML ) ) 
       expression += key.innerHTML;
-    if ( operators.test( key.innerHTML ) )
+    if ( operators.test( key.innerHTML ) && !operators.test( previousKey ) )
       expression += " " + key.innerHTML + " ";
     if ( key.innerHTML === "=" ) 
       expression = evaluateExpression( expression );
     if ( key.innerHTML === "C" ) 
       expression = "";
+    previousKey = key.innerHTML;
     text.innerHTML = expression;
   });
 });
