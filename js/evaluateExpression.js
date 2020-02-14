4c75bd6e082cb47bde5d68119bf41c5462b17e7f
benjorama <benmacomb@gmail.com>
2020-02-13 18:40:19 -0700

    Refactoring for readability.

--- js/evaluateExpression.js
+++ js/evaluateExpression.js
@@ -1,83 +1,87 @@
-import operate from "./operate.js";
-
-/**
- * Return the value of an infix expression.
- * @param {string} expression 
- */
-export default function evaluateExpression( expression ) {
-  return evaluatePostfix( convertToPostfix( expression ) );
-}
+import operate from './operate';
 
 /**
  * List of supported operators.
  */
-const operators = ["+", "-", "*", "/"];
+const OPERATORS = ['+', '-', '*', '/'];
 
 /**
- * Return an array of tokens in postfix order. 
- * @param {string} infixExpression 
- */
-function convertToPostfix( infixExpression ) {
-  let tokens = infixExpression.split( " " );
-  let postfixArray = [];
-  let stack = [];
-  tokens.forEach( token => {
-    if ( !operators.includes( token ) && token != "(" && token != ")" ) 
-      postfixArray.push( token );
-    if ( operators.includes( token ) ) {
-      if ( stack.length === 0 || stack[stack.length - 1] === "(" )
-        stack.push( token );
-      else {
-        while ( stack.length > 0 && stack[stack.length - 1] != "(" && 
-            getPrecedence( token ) <= getPrecedence( stack[stack.length - 1])) {
-          postfixArray.push( stack.pop() );
-        }
-        stack.push( token );
-      }
-    }
-    if ( token === "(")
-      stack.push( token );
-    if ( token === ")") {
-      while ( stack.length > 0 && stack[ stack.length - 1] != "(") {
-        postfixArray.push( stack.pop() );
-      }
-      stack.pop();
-    }
-  });
-  for ( let i = 0; i <= stack.length; i++ ) {
-    postfixArray.push( stack.pop() );  
+* Return the precedence of an operator.
+* @param {string} operator
+*/
+function getPrecedence(operator) {
+  if (operator === '*' || operator === '/') {
+    return 2;
   }
-  return postfixArray;
+  if (operator === '+' || operator === '-') {
+    return 1;
+  }
+  return 0;
 }
 
+/**
+ * Return an array of tokens in postfix order.
+ * @param {string} infixExpression
+ */
+function convertToPostfix(infixExpression) {
+  const TOKENS = infixExpression.split(' ');
+  const POSTFIX_ARRAY = [];
+  const OPERATOR_STACK = [];
+  TOKENS.forEach((token) => {
+    if (!OPERATORS.includes(token) && token !== '(' && token !== ')') {
+      POSTFIX_ARRAY.push(token);
+    }
+    if (OPERATORS.includes(token)) {
+      if (OPERATOR_STACK.length === 0 || OPERATOR_STACK[OPERATOR_STACK.length - 1] === '(') {
+        OPERATOR_STACK.push(token);
+      } else {
+        while (OPERATOR_STACK.length > 0 && OPERATOR_STACK[OPERATOR_STACK.length - 1] !== '('
+        && getPrecedence(token) <= getPrecedence(OPERATOR_STACK[OPERATOR_STACK.length - 1])) {
+          POSTFIX_ARRAY.push(OPERATOR_STACK.pop());
+        }
+        OPERATOR_STACK.push(token);
+      }
+    }
+    if (token === '(') {
+      OPERATOR_STACK.push(token);
+    }
+    if (token === ')') {
+      while (OPERATOR_STACK.length > 0 && OPERATOR_STACK[OPERATOR_STACK.length - 1] !== '(') {
+        POSTFIX_ARRAY.push(OPERATOR_STACK.pop());
+      }
+      OPERATOR_STACK.pop();
+    }
+  });
+  for (let i = 0; i <= OPERATOR_STACK.length; i += 1) {
+    POSTFIX_ARRAY.push(OPERATOR_STACK.pop());
+  }
+  return POSTFIX_ARRAY;
+}
 
 /**
  * Return the calculation of a postfix expression.
- * @param {Array} postfixArray 
+ * @param {Array} POSTFIX_ARRAY
  */
-function evaluatePostfix( postfixArray ) {
-  let stack = [];
-  postfixArray.forEach( token => {
-    if ( !operators.includes( token ) )
-      stack.push( token );
-    if ( operators.includes( token ) ) {
-      let a = stack.pop();
-      let b = stack.pop();
-      let result = operate( token, Number( b ), Number( a ) );
-      stack.push( result );
+function evaluatePostfix(POSTFIX_ARRAY) {
+  const OPERATOR_STACK = [];
+  POSTFIX_ARRAY.forEach((token) => {
+    if (!OPERATORS.includes(token)) {
+      OPERATOR_STACK.push(token);
+    }
+    if (OPERATORS.includes(token)) {
+      const A = OPERATOR_STACK.pop();
+      const B = OPERATOR_STACK.pop();
+      const RESULT = operate(token, Number(B), Number(A));
+      OPERATOR_STACK.push(RESULT);
     }
   });
-  return stack.pop();
+  return OPERATOR_STACK.pop();
 }
 
 /**
- * Return the precedence of an operator.
- * @param {string} operator 
+ * Return the value of an infix expression.
+ * @param {string} expression
  */
-function getPrecedence( operator ) {
-  if ( operator === "*" || operator === "/")
-    return 2;
-  if ( operator === "+" || operator === "-")
-    return 1;
-  return 0;
+export default function evaluateExpression(expression) {
+  return evaluatePostfix(convertToPostfix(expression));
 }
