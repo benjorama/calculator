fb1c72ea45b07d275dec040b3bbe60d917504d1d
benjorama <benmacomb@gmail.com>
2020-02-17 19:18:40 -0700

    Support clicking buttons with HTML entities.

--- js/calculator.js
+++ js/calculator.js
@@ -3,6 +3,12 @@ import evaluateExpression from './evaluateExpression';
 const OPERATORS = ['+', '-', '*', '/'];
 const KEYS = document.querySelectorAll('.key');
 const DISPLAY_TEXT = document.querySelector('.displayText');
+
+// Character codes for HTML entities.
+const DIVIDE = 247;
+const MULTIPLY = 215;
+const SUBTRACT = 8722;
+
 let expression = '';
 let previousKey = '';
 let previousCalculation = '';
@@ -34,6 +40,8 @@ function registerKey(key) {
   }
   if (key === 'AC') {
     expression = '';
+    previousKey = '';
+    previousCalculation = '';
   }
   if (key === 'CE') {
     expression = previousCalculation;
@@ -44,7 +52,15 @@ function registerKey(key) {
 
 KEYS.forEach((key) => {
   key.addEventListener('click', () => {
-    registerKey(key.innerHTML);
+    if (key.innerHTML.charCodeAt(0) === DIVIDE) {
+      registerKey(OPERATORS[3]);
+    } else if (key.innerHTML.charCodeAt(0) === MULTIPLY) {
+      registerKey(OPERATORS[2]);
+    } else if (key.innerHTML.charCodeAt(0) === SUBTRACT) {
+      registerKey(OPERATORS[1]);
+    } else {
+      registerKey(key.innerHTML);
+    }
   });
 });
 
